2740fdca94a85b9ae2c2c2aa18845b4b08c58282
CLAUDIO <codecadet@vonneumann.local>
2022-06-06 21:55:12 +0000

    images corrected

--- src/org/academiadecodigo/cunnilinux/jogodosfoda/Game.java
+++ src/org/academiadecodigo/cunnilinux/jogodosfoda/Game.java
@@ -9,8 +9,8 @@ public class Game {
     private Picture backgroundGame = new Picture(0, 0, "resources/backgroundGame.jpg");
 
 
-    private Picture heroDireita = new Picture(450, 60, "resources/claudioTiroDireita.png");
-    private Picture heroEsquerda = new Picture(450, 60, "resources/claudioTiroEsquerda.png");
+    private Picture heroDireita = new Picture(450, 60, "resources/claudioTiroEsquerda.png");
+    private Picture heroEsquerda = new Picture(450, 60, "resources/claudioTiroDireita.png");
 
 
     private Picture monsterTier1 = new Picture(400, 400, "resources/zombie.png");
