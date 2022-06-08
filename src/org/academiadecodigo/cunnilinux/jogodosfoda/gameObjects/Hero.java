922ccc309baf29daa14735f2fb2bb49f5f05fbfd
carlos-regis <zeca2078@gmail.com>
2022-06-08 10:59:50 +0000

    add Hero class

--- src/org/academiadecodigo/cunnilinux/jogodosfoda/gameObjects/Hero.java
+++ src/org/academiadecodigo/cunnilinux/jogodosfoda/gameObjects/Hero.java
@@ -1,4 +1,20 @@
 package org.academiadecodigo.cunnilinux.jogodosfoda.gameObjects;
 
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+
 public class Hero {
+
+    private Picture heroLeft;
+    private Picture heroRight;
+    private boolean dead;
+
+    public Hero(double x, double y) {
+
+        heroLeft = Picture(x, y, );
+        heroRight = Picture(x, y, );
+
+    }
+
+
+
 }
