b7b38cea23b239f8501dddfeabba79c42eefc9da
carlos-regis <zeca2078@gmail.com>
2022-06-08 20:53:46 +0000

    collisionDetector

--- src/org/academiadecodigo/cunnilinux/jogodosfoda/gameObjects/CollisionDetector.java
+++ src/org/academiadecodigo/cunnilinux/jogodosfoda/gameObjects/CollisionDetector.java
@@ -4,13 +4,11 @@ public class CollisionDetector {
 
     private static Hero hero;
     private static Enemy enemy;
-    private static Projectile[] projectiles;
+    private static Projectile projectiles;
 
     public static boolean intersectsEnemy() {
         return false; // hero.bounds().intersects(enemies.bounds());
     }
-
-
 
     public static void setHero(Hero hero) {
         CollisionDetector.hero = hero;
