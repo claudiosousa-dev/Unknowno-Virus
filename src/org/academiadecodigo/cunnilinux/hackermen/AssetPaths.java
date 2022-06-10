862ab289443b6cde32a0107a30d806e156e650d3
João <joaozito.lima2000@gmail.com>
2022-06-10 22:20:53 +0000

    monsters update and add boss graphics

--- src/org/academiadecodigo/cunnilinux/hackermen/AssetPaths.java
+++ src/org/academiadecodigo/cunnilinux/hackermen/AssetPaths.java
@@ -6,9 +6,12 @@ public class AssetPaths {
     public static final String HERO_LEFT = "resources/heroLeft.png";
     public static final String HERO_RIGHT = "resources/heroRight.png";
     public static final String MONSTER_TIER1 = "resources/monsterTier1.png";
-    public static final String BULLET = "resources/bullet.png";
+    public static final String BULLETLEFT = "resources/bulletLeft.png";
+    public static final String BULLETRIGHT = "resources/bulletRight.png";
     public static final String MONSTER_TIER2_LEFT = "resources/monsterTier2Left.png";
     public static final String MONSTER_TIER2_RIGHT = "resources/monsterTier2Right.png";
+    public static final String BOSS1LEFT = "resources/boss1Left.png";
+    public static final String BOSS1RIGHT = "resources/boss1Right.png";
     public static final String BULLET2 = "resources/bullet2.png";
     public static final String BULLET_FIRE = "resources/bulletFire.png";
     public static final String HEALTH = "resources/health.png";
