6bd69e5da07330db7b9e05422de14ad82df893ac
João <joaozito.lima2000@gmail.com>
2022-06-12 19:16:58 +0000

    update name

--- src/org/academiadecodigo/cunnilinux/hackermen/AssetPaths.java
+++ src/org/academiadecodigo/cunnilinux/hackermen/AssetPaths.java
@@ -4,7 +4,7 @@ public class AssetPaths {
 
     public static final String BACKGROUND_LEVEL1 = "resources/background1.jpg";
     public static final String BACKGROUND_LEVEL2 = "resources/background2.jpg";
-    public static final String START_MENU_WHITE = "resources/startMenuWhite.png";
+    public static final String START_MENU_WHITE = "resources/startMenuWhite2.png";
     public static final String START_MENU_RED = "resources/startMenuRed.png";
     public static final String VICTORY_MENU = "resources/victoryBackground.jpeg";
     public static final String HERO_LEFT = "resources/heroLeft.png";
