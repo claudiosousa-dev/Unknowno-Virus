1af4e55c50c87cb58e498c7fbaba1332f623bd9f
carlos-regis <zeca2078@gmail.com>
2022-06-12 00:02:11 +0000

    Refactor GameOverMenu class

--- src/org/academiadecodigo/cunnilinux/hackermen/utils/GameOverMenu.java
+++ src/org/academiadecodigo/cunnilinux/hackermen/utils/GameOverMenu.java
@@ -7,12 +7,10 @@ import org.academiadecodigo.simplegraphics.pictures.Picture;
 public class GameOverMenu {
 
     private final Picture gameOverBackground;
-    private boolean menu;
-    private Music gameOverMusic;
+    private final Music gameOverMusic;
 
     public GameOverMenu() {
 
-        menu = true;
         gameOverBackground = new Picture(Canvas.PADDING, Canvas.PADDING, AssetPaths.GAME_OVER);
         gameOverMusic = new Music(AssetPaths.GAME_OVER_VOICE);
 
@@ -38,8 +36,6 @@ public class GameOverMenu {
             }
 
         }
-
-        //gameOverMusic.stop();
 
     }
 }
